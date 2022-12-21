574d55860b7111ea3836c1351d6b1fa1a88b7d95
Nishantdubey11 <nishantdubey1195@gmail.com>
2022-12-21 18:59:32 +0530

    Login button

--- src/main/java/com/example/supply_chain_management_sys/HelloApplication.java
+++ src/main/java/com/example/supply_chain_management_sys/HelloApplication.java
@@ -1,7 +1,10 @@
 package com.example.supply_chain_management_sys;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -23,6 +26,11 @@ public class HelloApplication extends Application {
         Button searchButton = new Button("Search");
 
         GridPane gridPane = new GridPane();
+        gridPane.setMinSize(bodyPane.getMinWidth() , headerBar-10);
+        gridPane.setVgap(5);
+        gridPane.setHgap(5);
+
+        gridPane.setAlignment(Pos.CENTER);
 
         gridPane.add(searchText,0,0);
         gridPane.add(searchButton,1,0);
@@ -32,16 +40,37 @@ public class HelloApplication extends Application {
     private GridPane Loginpage()    {
         Label emailLabel = new Label("Email");
         Label passwordLabel = new Label("Password");
+        Label messageLabel = new Label("I am Message");
 
         TextField emailTextField = new TextField();
         PasswordField passwordField = new PasswordField();
 
-        GridPane gridPane = new GridPane();
+        Button loginButton = new Button("Login");
+        loginButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                String email = emailTextField.getText();
+                String password = passwordField.getText();
+                messageLabel.setText(email + "$" + password);
 
+            }
+        });
+
+        GridPane gridPane = new GridPane();
+        gridPane.setMinSize(bodyPane.getMinWidth() ,bodyPane.getMinHeight());
+        gridPane.setVgap(5);
+        gridPane.setHgap(5);
+      //  gridPane.setStyle("-fx-background-color: #C0C0C0");
+
+        gridPane.setAlignment(Pos.CENTER);
+
+        //first is X, second is Y
         gridPane.add(emailLabel,0,0);
         gridPane.add(emailTextField,1,0);
         gridPane.add(passwordLabel,0,1);
         gridPane.add(passwordField,1,1);
+        gridPane.add(loginButton, 0, 2);
+        gridPane.add(messageLabel,1,2);
 
         return gridPane;
     }
