68ee13710c056780933a06f9d7fed3eb259f24ce
Nishantdubey11 <nishantdubey1195@gmail.com>
2022-12-29 20:44:56 +0530

    Encrypted

--- src/main/java/com/example/supply_chain_management_sys/Supply_chain_management.java
+++ src/main/java/com/example/supply_chain_management_sys/Supply_chain_management.java
@@ -20,6 +20,7 @@ public class Supply_chain_management extends Application {
     public static final int width = 700, height = 600, headerBar = 50;
 
     Pane bodyPane = new Pane();
+    Login login = new Login();
     private GridPane headerBar(){
         TextField searchText = new TextField();
         Button searchButton = new Button("Search");
@@ -50,8 +51,12 @@ public class Supply_chain_management extends Application {
             public void handle(ActionEvent actionEvent) {
                 String email = emailTextField.getText();
                 String password = passwordField.getText();
-                messageLabel.setText(email + "$" + password);
-
+//                messageLabel.setText(email + "$" + password);
+                if (login.customerLogin(email, password)){
+                    messageLabel.setText("Login Successful");
+                }else {
+                    messageLabel.setText("Login Failed");
+                }
             }
         });
 
